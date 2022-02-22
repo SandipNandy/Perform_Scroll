526850ac9eca5668c0498531a2fca06c0433a2de
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2022-02-22 09:23:04 +0530

    2nd commit

--- Perform_Scroll.java
+++ Perform_Scroll.java
@@ -12,7 +12,7 @@ import org.testng.Assert;
 public class Perform_Scroll {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","..\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
